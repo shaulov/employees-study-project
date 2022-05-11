b264a6803803f52e3cb70250cdee39fc856b5c74
shaulov <shineginn@gmail.com>
2022-05-11 11:26:09 +0300

    Add function for adding employees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,12 +18,29 @@ class App extends Component {
                 {name: 'Elen Notthem', salary: 2700, increase: false, id: 3},
             ]
         }
+        this.maxId = 4;
     }
 
     deleteItem = (id) => {
         this.setState(({data}) => {
             return {
                 data: data.filter(item => item.id !== id)
+            }
+        });
+    }
+
+    addItem = (name, salary) => {
+        const newItem = {
+            name,
+            salary,
+            increase: false,
+            id: this.maxId++,
+        }
+
+        this.setState(({data}) => {
+            const newArr = [...data, newItem];
+            return {
+                data: newArr,
             }
         });
     }
@@ -41,7 +58,7 @@ class App extends Component {
                 <EmployeesList 
                     data={this.state.data}
                     onDelete={this.deleteItem}/>
-                <EmployeesAddForm/>
+                <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
     }
