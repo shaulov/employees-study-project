c0e80c62ecdd47da11b4a0d82f22f14d73f0fb14
shaulov <shineginn@gmail.com>
2022-05-11 17:19:31 +0300

    Add employees counting

--- src/components/app-info/app-info.js
+++ src/components/app-info/app-info.js
@@ -1,11 +1,13 @@
 import './app-info.css';
 
-const AppInfo = () => {
+const AppInfo = (props) => {
+    const {employeesCount, employeesIncrease} = props;
+    
     return (
         <div className="app-info">
             <h1>Учёт сотрудников в компании Nekk</h1>
-            <h2>Общее число сотрудников: </h2>
-            <h2>Премию получат: </h2>
+            <h2>Общее число сотрудников: {employeesCount}</h2>
+            <h2>Премию получат: {employeesIncrease}</h2>
         </div>
     );
 }
