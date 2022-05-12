af98223de944e529c29f491212263e812cfb8844
shaulov <shineginn@gmail.com>
2022-05-12 20:27:19 +0300

    Add changing salary in data

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -92,6 +92,17 @@ class App extends Component {
         this.setState({filter})
     }
 
+    onChangeSalary = (id, value) => {
+        this.setState(({data}) => ({
+            data: data.map(item => {
+                if (item.id === id) {
+                    return {...item, salary: value}
+                }
+                return item;
+            })
+        }))
+    }
+
     render() {
         const {data, term, filter} = this.state;
         const employees = data.length;
@@ -112,7 +123,8 @@ class App extends Component {
                 <EmployeesList 
                     data={visibleData}
                     onDelete={this.deleteItem}
-                    onToggleProp={this.onToggleProp}/>
+                    onToggleProp={this.onToggleProp}
+                    onChangeSalary={this.onChangeSalary}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
             </div>
         );
--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,7 +1,7 @@
 import './employees-list-item.css';
 
 const EmployeesListItem = (props) => {
-    const {name, salary, onDelete, onToggleProp, increase, rise} = props;
+    const {name, salary, onDelete, onToggleProp, onChangeSalary, increase, rise} = props;
 
     let classNames = "list-group-item d-flex justify-content-between";
     if (increase) {
@@ -16,7 +16,10 @@ const EmployeesListItem = (props) => {
             <span className="list-group-item-label"
                 onClick={onToggleProp}
                 data-toggle="rise">{name}</span>
-            <input type="text" className="list-group-item-input" defaultValue={salary + '$'}/>
+            <input type="text" 
+                className="list-group-item-input" 
+                defaultValue={salary + '$'}
+                onChange={onChangeSalary}/>
             <div className='d-flex justify-content-center align-items-center'>
                 <button type="button"
                     className="btn-cookie btn-sm "
