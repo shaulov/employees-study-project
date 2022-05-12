af20eb5b151385ecbf0db2087f275373232747ec
shaulov <shineginn@gmail.com>
2022-05-12 17:46:08 +0300

    Add search functional

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
                 {name: 'John Krasinski', salary: 800, increase: false, rise: true, id: 1},
                 {name: 'Luke Lambic', salary: 1500, increase: true, rise: false, id: 2},
                 {name: 'Elen Notthem', salary: 2700, increase: false, rise: false, id: 3},
-            ]
+            ],
+            term: '',
         }
         this.maxId = 4;
     }
@@ -57,9 +58,25 @@ class App extends Component {
         }))
     }
 
+    searchEmp = (items, term) => {
+        if (term.length === 0) {
+            return items;
+        }
+
+        return items.filter(item => {
+            return item.name.indexOf(term) > -1;
+        });
+    }
+
+    onUpdateSearch = (term) => {
+        this.setState({term});
+    }
+
     render() {
-        const employees = this.state.data.length;
-        const increased = this.state.data.filter(item => item.increase).length;
+        const {data, term} = this.state;
+        const employees = data.length;
+        const increased = data.filter(item => item.increase).length;
+        const visibleData = this.searchEmp(data, term);
 
         return (
             <div className="app">
@@ -68,12 +85,12 @@ class App extends Component {
                     employeesIncrease={increased}/>
     
                 <div className="search-panel">
-                    <SearchPanel/>
+                    <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
                     <AppFilter/>
                 </div>
     
                 <EmployeesList 
-                    data={this.state.data}
+                    data={visibleData}
                     onDelete={this.deleteItem}
                     onToggleProp={this.onToggleProp}/>
                 <EmployeesAddForm onAdd={this.addItem}/>
