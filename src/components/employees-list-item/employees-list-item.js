0ca98aad0acddb5ba91c64e6f8fa072167a98b06
shaulov <shineginn@gmail.com>
2022-05-13 17:16:30 +0300

    Add space down handler for component

--- src/components/employees-list-item/employees-list-item.js
+++ src/components/employees-list-item/employees-list-item.js
@@ -1,42 +1,55 @@
+import { Component } from 'react';
+
 import './employees-list-item.css';
 
-const EmployeesListItem = (props) => {
-    const {name, salary, onDelete, onToggleProp, onChangeSalary, increase, rise} = props;
+class EmployeesListItem extends Component{
 
-    let classNames = "list-group-item d-flex justify-content-between";
-    if (increase) {
-        classNames += ' increase';
+    onSpaceDown = (e) => {
+        if (e.keyCode === 32) {
+            this.props.onToggleProp(e);
+        }
     }
-    if (rise) {
-        classNames += ' like';
-    }
+    
+    render() {
+        const {name, salary, onDelete, onToggleProp, onChangeSalary, increase, rise} = this.props;
 
-    return (
-        <li className={classNames}>
-            <span className="list-group-item-label"
-                onClick={onToggleProp}
-                data-toggle="rise">{name}</span>
-            <input type="text" 
-                className="list-group-item-input" 
-                defaultValue={salary + '$'}
-                onChange={onChangeSalary}/>
-            <div className='d-flex justify-content-center align-items-center'>
-                <button type="button"
-                    className="btn-cookie btn-sm "
+        let classNames = "list-group-item d-flex justify-content-between";
+        if (increase) {
+            classNames += ' increase';
+        }
+        if (rise) {
+            classNames += ' like';
+        }
+
+        return (
+            <li className={classNames}>
+                <span className="list-group-item-label"
                     onClick={onToggleProp}
-                    data-toggle="increase">
-                    <i className="fas fa-cookie"></i>
-                </button>
+                    data-toggle="rise"
+                    tabIndex={0}
+                    onKeyDown={this.onSpaceDown}>{name}</span>
+                <input type="text" 
+                    className="list-group-item-input" 
+                    defaultValue={salary + '$'}
+                    onChange={onChangeSalary}/>
+                <div className='d-flex justify-content-center align-items-center'>
+                    <button type="button"
+                        className="btn-cookie btn-sm "
+                        onClick={onToggleProp}
+                        data-toggle="increase">
+                        <i className="fas fa-cookie"></i>
+                    </button>
 
-                <button type="button"
-                        className="btn-trash btn-sm "
-                        onClick={onDelete}>
-                    <i className="fas fa-trash"></i>
-                </button>
-                <i className="fas fa-star"></i>
-            </div>
-        </li>
-    );
+                    <button type="button"
+                            className="btn-trash btn-sm "
+                            onClick={onDelete}>
+                        <i className="fas fa-trash"></i>
+                    </button>
+                    <i className="fas fa-star"></i>
+                </div>
+            </li>
+        );
+    }
 }
 
 export default EmployeesListItem;
